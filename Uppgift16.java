05ae6d11020ea2cff420a1a25fb17581f14ef079
BuildTools <unconfigured@null.spigotmc.org>
2017-09-11 09:40:01 +0200

    Första uppgiften

---
+++ Uppgift16.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Uppgift16{
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"Christoffer");
+
+        JOptionPane.showMessageDialog(null,"0705772297" );
+    }
+			} 
